e25be8b9af9ee90a1dfe0d516073249ce0712baf
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-04-06 16:20:41 -0300

    feat: criação de prompt para fazer resumo da conversa

--- page_1.py
+++ page_1.py
@@ -14,11 +14,46 @@ from dotenv import load_dotenv, find_dotenv
 PASTA_ARQUIVOS = Path(__file__).parent / 'arquivos'
 PASTA_ARQUIVOS.mkdir(exist_ok=True)
 
+PROMPT = '''
+Faça um resumo do texto delimitado por ####
+O texto é a transcrição de uma conversa formal / informal.
+O resumo deve contar com os principais assuntos discutidos na conversa.
+O resumo deve ter no maximo 1000 caracteres.
+O resumo deve estar em texto corrido.
+No final, devem ser apresentados os principais pontos discutidos na conversa no formato de bullet points.
+
+o formato final que eu desejo é o seguinte:
+
+Resumo conversa:
+ - escrever aqui o resumo.
+
+ principais pontos discutidos:
+  - ponto 1
+  - ponto 2
+  - ponto 3
+  - ponto n
+
+
+
+texto: ####{}####
+
+
+'''
+
 _ = load_dotenv(find_dotenv())
 
 def salva_arquivo(caminho_arquivo, conteudo):
     with open(caminho_arquivo, 'w') as f:
         f.write(conteudo)
+
+def le_arquivo(caminho_arquivo):
+    if caminho_arquivo.exists():
+        with open(caminho_arquivo) as f:
+            return f.read()
+    else:
+        return ''
+
+
 
 def listar_reunioes():
     lista_reunioes = PASTA_ARQUIVOS.glob('*')
@@ -29,6 +64,9 @@ def listar_reunioes():
         data_reuniao = pasta_reuniao.stem
         ano, mes, dia, hora, min, seg = data_reuniao.split('-')
         reunioes_dict[data_reuniao] = f'{ano}-{mes}-{dia} {hora}:{min}:{seg}'
+        titulo = le_arquivo(pasta_reuniao / 'titulo.txt')
+        if titulo != '':
+            reunioes_dict[data_reuniao] += f' - {titulo}'
     return reunioes_dict
 
 # OPENAI UTILS =====================
@@ -49,7 +87,7 @@ def chat_openai(
         modelo='gpt-4o-mini-2024-07-18',
     ):
     mensagens = [{'role': 'user', 'content': mensagem}]
-    reposta = client.chat.completions.create(
+    resposta = client.chat.completions.create(
         model=modelo,
         messages=mensagens,
         )
@@ -72,7 +110,7 @@ def tab_grava_reuniao():
     webrtx_ctx = webrtc_streamer(
         key='recebe_audio',
         mode=WebRtcMode.SENDONLY,
-        audio_receiver_size=1024,
+        audio_receiver_size=4096,
         media_stream_constraints={
             'audio': True,
             'video': False,
@@ -103,10 +141,10 @@ def tab_grava_reuniao():
                 continue
             audio_completo = adiciona_chunck_audio(frames_de_audio, audio_chunck)
             audio_chunck = adiciona_chunck_audio(frames_de_audio, audio_chunck)
-            if len(audio_chunck) > 0:
+            if len(audio_chunck) > 0 and audio_chunck.dBFS > -30:
                 audio_completo.export(pasta_reuniao / 'audio.mp3')   
                 agora = time.time()
-                if agora - ultima_transcricao > 5:
+                if agora - ultima_transcricao > 15:
                     ultima_transcricao = agora
                     audio_chunck.export(pasta_reuniao / 'audio_temp.mp3')
                     transcricao_chunck = transcreve_audio(pasta_reuniao / 'audio_temp.mp3')
@@ -125,8 +163,44 @@ def tab_grava_reuniao():
 # TAB SELEÇÃO REUNIÃO =====================
 def tab_selecao_reuniao():
      reunioes_dict = listar_reunioes()
-     st.write(reunioes_dict)
+     if len(reunioes_dict) > 0:
+        reunioao_selecionada = st.selectbox('Selecione uma reunião', 
+                                         list(reunioes_dict.values()))
+        st.divider()
+        reuniao_data = [k for k, v in reunioes_dict.items() if v == reunioao_selecionada][0]
+        pasta_reuniao = PASTA_ARQUIVOS / reuniao_data
+        if not (pasta_reuniao / 'titulo.txt').exists():
+            st.warning('Adicione um titulo para a reunião')
+            titulo_reuniao = st.text_input('Título da reunião', key='titulo_input')
+            if st.button('Salvar'):
+                salvar_titulo(pasta_reuniao, st.session_state.titulo_input)
+                st.success('Título salvo com sucesso!')
+                st.rerun()
 
+            # st.button('Salvar',
+            #           on_click=salvar_titulo,
+            #           args=(pasta_reuniao, titulo_reuniao))
+        else:
+            titulo = le_arquivo(pasta_reuniao / 'titulo.txt')
+            transcricao = le_arquivo(pasta_reuniao / 'transcricao.txt')
+            resumo = le_arquivo(pasta_reuniao / 'resumo.txt')
+            if resumo == '':
+                gerar_resumo(pasta_reuniao)
+                resumo = le_arquivo(pasta_reuniao / 'resumo.txt')
+            
+            st.markdown(f' ## {titulo}')            
+            st.markdown(f' Transcrição: {transcricao}')
+            st.markdown(f' Resumo: {resumo}')
+
+def salvar_titulo(pasta_reuniao, titulo):
+    salva_arquivo(pasta_reuniao / 'titulo.txt' , titulo)
+   
+def gerar_resumo(pasta_reuniao):
+    transcricao = le_arquivo(pasta_reuniao / 'transcricao.txt')
+    resumo = chat_openai(mensagem = PROMPT.format(transcricao))  
+    salva_arquivo(pasta_reuniao / 'resumo.txt', resumo)  
+
+    pass
 # MAIN =====================
 def main():
     st.header('Bem-vindo ao ChatToNote 🎙️', divider=True)
